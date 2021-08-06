87520b4014f0ed5747d17cc6b1d89985efe54ba5
Александр Новиков <anovikov@tsum.ru>
2021-08-06 15:58:31 +0300

    user gRPC gateway

--- README.md
+++ README.md
@@ -16,3 +16,38 @@
 
     ./chat_server
     ./chat_client
+
+
+
+
+    brew tap bufbuild/buf
+    brew install buf
+    
+    go install \
+        github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway \
+        github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2 \
+        google.golang.org/protobuf/cmd/protoc-gen-go \
+        google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
+    cd user/client
+    go mod tidy
+    buf generate
+
+    cd user/server
+    go mod tidy
+    buf generate
+
+    cd user/client
+    go mod tidy
+    go build -o ../user_client
+
+    cd user/server
+    go mod tidy
+    go build -o ../user_server
+
+    ./user_server
+
+    curl http://localhost:8080/v1/user/john -H "Grpc-Metadata-password: go"
+    curl http://localhost:8080/v1/user -H "Grpc-Metadata-password: go" -d "{\"userId\":\"John\",\"email\":\"john@gmail.com\"}"
+
+    ./user_client
--- user/server/main.go
+++ user/server/main.go
@@ -3,21 +3,67 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	pb "github.com/mem-memov/buildsystemswithgo/user/server/user"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 	"net"
+	"net/http"
 )
 
 type UserServer struct {
+	httpAddr string
+	grpcAddr string
 	pb.UnimplementedUserServiceServer
 }
 
 func (u *UserServer) GetUser(ctx context.Context, req *pb.UserRequest) (*pb.User, error) {
-	fmt.Println("Server received: ", req.String())
+	fmt.Println("Server received (GetUser): ", req.String())
 	return &pb.User{UserId: "John", Email: "john@gmail.com"}, nil
+}
+
+func (u *UserServer) Create(ctx context.Context, req *pb.User) (*pb.User, error) {
+	fmt.Println("Server received (Create): ", req.String())
+	return &pb.User{UserId: "John", Email: "john@gmail.com"}, nil
+}
+
+func (u *UserServer) ServeGrpc() {
+	lis, err := net.Listen("tcp", u.grpcAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer(grpc.UnaryInterceptor(AuthServerInterceptor))
+
+	pb.RegisterUserServiceServer(s, u)
+	fmt.Println("Server listening GRPC:")
+
+	if err := s.Serve(lis); err != nil {
+		panic(err)
+	}
+}
+
+func (u *UserServer) ServeHttp() {
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	endpoint := u.grpcAddr
+
+	err := pb.RegisterUserServiceHandlerFromEndpoint(context.Background(), mux, endpoint, opts)
+	if err != nil {
+		panic(err)
+	}
+
+	httpServer := &http.Server{
+		Addr: u.httpAddr,
+		Handler: mux,
+	}
+
+	fmt.Println("Server listening HTTP:")
+	if err := httpServer.ListenAndServe(); err != nil {
+		panic(err)
+	}
 }
 
 func AuthServerInterceptor(
@@ -45,15 +91,8 @@ func AuthServerInterceptor(
 }
 
 func main() {
-	lis, err := net.Listen("tcp", "localhost:50051")
-	if err != nil {
-		panic(err)
-	}
-
-	s := grpc.NewServer(grpc.UnaryInterceptor(AuthServerInterceptor))
-	pb.RegisterUserServiceServer(s, &UserServer{})
-
-	if err := s.Serve(lis); err != nil {
-		panic(err)
-	}
+	us := UserServer{httpAddr: ":8080", grpcAddr: ":50051"}
+	go us.ServeGrpc()
+	us.ServeHttp()
 }
+
